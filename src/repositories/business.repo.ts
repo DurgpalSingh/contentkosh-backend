50865ed67eee15f6814a370e78e06db8a9b65de1
Aaditya <aaditya2110s@gmail.com>
2026-02-07 12:04:04 +0530

    Resolved all comments Updated seed data

--- src/repositories/business.repo.ts
+++ src/repositories/business.repo.ts
@@ -1,6 +1,9 @@
 import { Prisma } from '@prisma/client';
 import { prisma } from '../config/database';
 
+export type BusinessCreateInput = Prisma.BusinessCreateInput;
+export type BusinessUpdateInput = Prisma.BusinessUpdateInput;
+
 export async function createBusiness(data: Prisma.BusinessCreateInput) {
   return await prisma.business.create({
     data
